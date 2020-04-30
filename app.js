b9038397b5bab5ea287c99138133d8a5739198c5
vbk-dev <vbkwork97@gmail.com>
2020-04-30 07:49:12 +0530

    heroku first deploy 3 try

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const questionsRoutes = require('./routes/question-routes');
 const answerRouter = require('./routes/answer-routes');
 const voteRoutes = require('./routes/vote-routes');
 
-const PORT = process.env.PORT || 5000;
-
 const app = express();
 
 app.use(express.json({extended: false}));
@@ -21,12 +19,15 @@ app.use('/api/answer', answerRouter);
 app.use('/api/vote', voteRoutes);
 
 if (process.env.NODE_ENV === 'production') {
+    // Set static folder
     app.use(express.static('client/build'));
-
+  
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
+
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true })
     .then(result => app.listen(PORT, () => console.log(`server running at port ${PORT}.....`)))
