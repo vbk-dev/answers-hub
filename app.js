f2566a48140989212f5cad078c8ee8687b51bad3
vbk-dev <vbkwork97@gmail.com>
2020-04-30 07:53:57 +0530

    heroku first deploy 4 try

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-const PORT = process.env.PORT || 5000;
+// const PORT = ;
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true })
-    .then(result => app.listen(PORT, () => console.log(`server running at port ${PORT}.....`)))
+    .then(result => app.listen(process.env.PORT || 5000, () => console.log(`server running.....`)))
     .catch(err => console.log(err));
